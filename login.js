b48b62560c750d803d4d927ef0b72f6e17bb8bbf
Andrew <andrewhughes1988@gmail.com>
2020-03-24 11:18:15 -0500

    small refactoring

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/User.model');
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
 
     const email = req.body.email;
     const password = req.body.password;
